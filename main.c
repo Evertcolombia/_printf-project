c35ff2e178e4394e776f0dbe779a353ece9559ec
evertcolombia <evertescalante@gmail.com>
2019-11-03 02:09:21 +0000

    compile and functional with char format

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main (void)
+{
+	char a = 'H';
+	
+	printf("Expected: %c\n", a);
+	_printf("Result: %c\n", a);
+
+	return (0);
+}
